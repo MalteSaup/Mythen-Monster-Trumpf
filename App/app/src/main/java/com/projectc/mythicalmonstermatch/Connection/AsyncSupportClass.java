d32011c550a272a017e8e46cd3db72d115bb0e10
MalteSaup <Malte.Saupe>
2020-06-08 12:43:48 +0200

    ö

---
+++ App/app/src/main/java/com/projectc/mythicalmonstermatch/Connection/AsyncSupportClass.java
@@ -0,0 +1,28 @@
+package com.projectc.mythicalmonstermatch.Connection;
+
+import android.os.AsyncTask;
+
+public class AsyncSupportClass {
+    public AsyncSupportClass(){
+    }
+    public void sendMessage(final ServerListener sL, final String msg){
+        AsyncTask asyncTask = new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                sL.sendMessage(msg);
+                return null;
+            }
+        };
+        asyncTask.execute();
+    }
+    public void sendMessage(final Client client, final String msg){
+        AsyncTask asyncTask = new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                client.sendMessage(msg);
+                return null;
+            }
+        };
+        asyncTask.execute();
+    }
+}
