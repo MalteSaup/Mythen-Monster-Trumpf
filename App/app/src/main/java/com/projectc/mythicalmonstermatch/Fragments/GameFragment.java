54f50d3bb7512a569fb2b04a05b70585be64591f
LAPTOP-OC8K68GC\G-wan <theciwan@gmail.com>
2020-07-07 13:53:26 +0200

    Revert "Revert "Delayed the indication of ones own win for extra suspense""
    This reverts commit 3c45c5b8c0d02f36bc5ababee54f16cd866192ed.

--- App/app/src/main/java/com/projectc/mythicalmonstermatch/Fragments/GameFragment.java
+++ App/app/src/main/java/com/projectc/mythicalmonstermatch/Fragments/GameFragment.java
@@ -422,9 +422,6 @@ public class GameFragment extends Fragment {
         isPlaying = true;
         gA.runOnUiThread(new Runnable() {
             public void run(){
-                if (winnersToBeMarked.charAt(0)=='1'){
-                    playerTextViews[0].setBackgroundColor(Color.GREEN);
-                }
                 for (int i = 0; i < playerCount-1; i++){
                     Log.d("tokens2",""+enemyCardsToDisplay[i]);
                     Log.d("stupide", winnersToBeMarked + " | " + winnersToBeMarked.charAt(i+1) + " " + ('1'== winnersToBeMarked.charAt(i+1)));
@@ -470,6 +467,7 @@ public class GameFragment extends Fragment {
                         flipAllCards(true);
                     }
                 });
+
                 return null;
             }
         };
@@ -478,6 +476,7 @@ public class GameFragment extends Fragment {
         AsyncTask markWinners = new AsyncTask() {
             @Override
             protected Object doInBackground(Object[] objects) {
+
                 try {
                     Thread.sleep(1000);
                 } catch (Exception e){
@@ -487,7 +486,9 @@ public class GameFragment extends Fragment {
                 gA.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        //markWinner();
+                        if (winnersToBeMarked.charAt(0)=='1'){
+                            playerTextViews[0].setBackgroundColor(Color.GREEN);
+                        }
                     }
                 });
                 try {
